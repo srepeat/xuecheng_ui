de22fcad4e73e79e8cd0f78f102e90f4c9276e24
DaHey <haha_jcet@163.com>
2019-12-31 11:46:41 +0800

    xc_ui

---
+++ xc-ui-pc-leanring/config/sysConfig.js
@@ -0,0 +1,12 @@
+var sysConfig = {
+    xcApiUrlPre: '/api',//客户端请求
+    staticURL: '/static',
+    backApiURL: 'http://www.xucheng.com/openapi',//服务端请求
+    backStaticURL: 'http://www.xucheng.com/static',
+    imgUrl:'http://img.xucheng.com/',
+    videoUrl:'http://video.xucheng.com/video/',
+    openAuthenticate:true,
+    openAuthorize:true
+}
+
+module.exports = sysConfig
